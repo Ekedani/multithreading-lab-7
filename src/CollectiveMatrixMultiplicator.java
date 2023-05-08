9dd9bc9c4579fa5246dc1e621ba3bbdb0385747f
Vladlen <ilusenkovladlen@gmail.com>
2023-05-08 21:54:28 +0300

    Initial commit

---
+++ src/CollectiveMatrixMultiplicator.java
@@ -0,0 +1,9 @@
+import mpi.MPI;
+
+public class CollectiveMatrixMultiplicator {
+    public static void main(String[] args) {
+        MPI.Init(args);
+        //TODO: Implement logic here
+        MPI.Finalize();
+    }
+}
