9570643ef57444ced25613a0066d6d263c81635e
Vladlen <ilusenkovladlen@gmail.com>
2023-05-09 13:24:43 +0300

    Started implementing logic

--- src/CollectiveMatrixMultiplicator.java
+++ src/CollectiveMatrixMultiplicator.java
@@ -18,26 +18,33 @@ public class CollectiveMatrixMultiplicator {
         MPI.Init(args);
         taskId = MPI.COMM_WORLD.Rank();
         tasksNumber = MPI.COMM_WORLD.Size();
-
+        long startTime = 0, endTime;
         if (taskId == MASTER) {
-            System.out.println("MPI_BMM has started with " + tasksNumber + " tasks.");
+            System.out.println("MPI_CMM has started with " + tasksNumber + " tasks.");
             Helper.initializeMatrixWithNumber(a, 10);
             Helper.initializeMatrixWithNumber(b, 10);
+            startTime = System.nanoTime();
+        }
 
-            long startTime = System.nanoTime();
+        // MPI.COMM_WORLD.Scatter();
+        MPI.COMM_WORLD.Bcast(b, 0, NUMBER_OF_COLS_IN_A, MPI.OBJECT, MASTER);
+        /*for (int k = 0; k < NUMBER_OF_COLS_IN_B; k++) {
+            for (int i = 0; i < rows[0]; i++) {
+                for (int j = 0; j < NUMBER_OF_COLS_IN_A; j++) {
+                    c[i][k] += a[i][j] * b[j][k];
+                }
+            }
+        }*/
+        // MPI.COMM_WORLD.Gather();
 
-            //TODO: Implement logic here
-
-            long endTime = System.nanoTime();
+        if (taskId == MASTER) {
+            endTime = System.nanoTime();
             System.out.println("===== RESULTS =====");
             if (RESULT_IS_PRINTED) {
                 Helper.outputMatrix(c);
             }
             System.out.println("Execution time: " + (endTime - startTime) + " ns");
         }
-
-        //TODO: Implement logic here
-
         MPI.Finalize();
     }
 }
