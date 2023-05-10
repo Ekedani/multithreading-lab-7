4abb273c85e4d39d4f96d2e47bd4524103ed469b
Vladlen <ilusenkovladlen@gmail.com>
2023-05-10 14:02:05 +0300

    Implemented multiplication logic

--- src/CollectiveMatrixMultiplicator.java
+++ src/CollectiveMatrixMultiplicator.java
@@ -6,7 +6,7 @@ public class CollectiveMatrixMultiplicator {
     public static int NUMBER_OF_COLS_IN_B = 1000;
     public static int MASTER = 0;
 
-    public static boolean RESULT_IS_PRINTED = true;
+    public static boolean RESULT_IS_PRINTED = false;
 
     public static void main(String[] args) {
         int taskId, tasksNumber;
@@ -26,16 +26,51 @@ public class CollectiveMatrixMultiplicator {
             startTime = System.nanoTime();
         }
 
-        // MPI.COMM_WORLD.Scatter();
+        int averow = NUMBER_OF_ROWS_IN_A / tasksNumber;
+        int extra = NUMBER_OF_ROWS_IN_A % tasksNumber;
+        var rowsCounts = new int[tasksNumber];
+        var rowsOffsets = new int[tasksNumber];
+        for (int i = 0; i < tasksNumber; i++) {
+            rowsCounts[i] = i < extra ? averow + 1 : averow;
+            rowsOffsets[i] = i == 0 ? 0 : rowsOffsets[i - 1] + rowsCounts[i - 1];
+        }
+        var rowsInTask = rowsCounts[taskId];
+
+        var aRowsBuffer = new double[rowsInTask][NUMBER_OF_COLS_IN_A];
+        MPI.COMM_WORLD.Scatterv(
+                a,
+                0,
+                rowsCounts,
+                rowsOffsets,
+                MPI.OBJECT,
+                aRowsBuffer,
+                0,
+                rowsInTask,
+                MPI.OBJECT,
+                MASTER
+        );
+
         MPI.COMM_WORLD.Bcast(b, 0, NUMBER_OF_COLS_IN_A, MPI.OBJECT, MASTER);
-        /*for (int k = 0; k < NUMBER_OF_COLS_IN_B; k++) {
-            for (int i = 0; i < rows[0]; i++) {
+        var cRowsBuffer = new double[rowsInTask][NUMBER_OF_COLS_IN_B];
+        for (int k = 0; k < NUMBER_OF_COLS_IN_B; k++) {
+            for (int i = 0; i < rowsInTask; i++) {
                 for (int j = 0; j < NUMBER_OF_COLS_IN_A; j++) {
-                    c[i][k] += a[i][j] * b[j][k];
+                    cRowsBuffer[i][k] += aRowsBuffer[i][j] * b[j][k];
                 }
             }
-        }*/
-        // MPI.COMM_WORLD.Gather();
+        }
+
+        MPI.COMM_WORLD.Gatherv(
+                cRowsBuffer,
+                0, rowsInTask,
+                MPI.OBJECT,
+                c,
+                0,
+                rowsCounts,
+                rowsOffsets,
+                MPI.OBJECT,
+                MASTER
+        );
 
         if (taskId == MASTER) {
             endTime = System.nanoTime();
